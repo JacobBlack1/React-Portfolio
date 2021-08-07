fac13578dfe740ea041b2abd96036cc6b3c1e5cd
unknown <jacobchrisb@gmail.com>
2021-08-07 10:57:46 -0400

    Header.js

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+       <div className="header">
+           <h1>Jacob Black: 2021-Portfolio</h1>
+       </div>
+    )
+}
