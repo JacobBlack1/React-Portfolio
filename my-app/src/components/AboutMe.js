58ab8f1ac70ea9a78fd75c5b4b8eeae5c0e40baa
unknown <jacobchrisb@gmail.com>
2021-08-07 10:41:38 -0400

    AboutMe.js

---
+++ my-app/src/components/AboutMe.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Main.css";
+
+
+function AboutMe() {
+    return (
+      <main>
+        <h2>About Me</h2>
+        <p> My name is Jacob Black, Im 19 years old and from 
+            Richmond Va. 
+            I am youthful and full of ambision and new ideas. 
+            I spend alot of time working and reading to absorb 
+            new in formation, to help me in the future. 
+            I always strive for success.
+        </p>
+
+        </main>
+    );
+}
+
+export default AboutMe;
