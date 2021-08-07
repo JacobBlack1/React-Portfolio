7a31aaf1ec2b4274542e28668a751d29ac4c127f
unknown <jacobchrisb@gmail.com>
2021-08-07 10:50:36 -0400

    contact.js

---
+++ my-app/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>I Apreciate you Viewing my portfolio:</h1>
+
+      <p>For Any Questions and or thoughts, please contact me:</p>
+      <h3>
+        {" "}
+        <a href="mailto:Jacobchrisb@gmail.com">Email:</a>
+        <span> </span>| Phone: 804-640-2161|{" "}
+        <a href="https://github.com/JacobBlack1">GitHub</a>
+      </h3>
+    </div>
+  );
+};
+
+export default Contact;
+ 
+
