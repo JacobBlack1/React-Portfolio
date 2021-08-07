f2846a9e7ea8e719734d9a3c76ace941e5553c5f
unknown <jacobchrisb@gmail.com>
2021-08-07 10:32:18 -0400

    index.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Project from "./components/Project";
+import Resume from "./components/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router basename="/react-portfolio">
+        <Header />
+        <Route exact path="/">
+          <AboutMe />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/project">
+          <Project />
+        </Route>
+        <Route exact path="/resume">
+          <Resume />
+        </Route>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
