189f25ec0a93db6bab7000a4185ffb2672af49a3
unknown <jacobchrisb@gmail.com>
2021-08-07 11:52:42 -0400

    js

---
+++ my-app/src/components/Project.js
@@ -0,0 +1,26 @@
+mport "../styles/Project.css";
+
+function Project() {
+  return (
+   <div>
+    <div className="row">
+      <div className="col cd-left cd-one" id="cardBdOne">
+        <h2 className="cd-text-title-1" type="button">
+        Project 1
+        </h2>
+        <a 
+          className="cd-text"
+          href="https://jacobblack1.github.io/Homework_3/"
+          type="button"
+        >
+         Password Generator
+            March 8th
+        </a>
+    </div>
+       
+           
+
+ 
+
+
+export default Project;
